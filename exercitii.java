2bed29caa57690c4d7a0e6f5c1e6044f97fe2ba9
eric <ericpredit@gmail.com>
2025-03-04 11:33:16 +0200

    first commit

---
+++ exercitii.java
@@ -0,0 +1,12 @@
+package tema_curs2;
+
+public class exercitii {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!");
+		
+	}
+
+}
