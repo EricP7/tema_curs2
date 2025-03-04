669595885ee87fcdfd10ebbcac5e486be72bdba1
eric <ericpredit@gmail.com>
2025-03-04 11:48:56 +0200

    problema1 terminata

--- exercitii.java
+++ exercitii.java
@@ -1,11 +1,26 @@
 package tema_curs2;
 
+import java.util.Scanner;
+
 public class exercitii {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//problema1
+		System.out.println("Problema 1:\n");
+
+		Scanner inputObj = new Scanner(System.in);
+		System.out.println("scrie un numar: ");
+		int nr1Prob1 = inputObj.nextInt();
+		if(nr1Prob1 >= 1 && nr1Prob1 <= 10)
+			if(nr1Prob1 > 5)
+				System.out.println("ADMIS\n");
+			else
+				System.out.println("RESPINS\n");
+		else
+			System.out.println("INVALID\n");
 		
-		System.out.println("Hello World!");
+		//problema2
+
 		
 	}
 
