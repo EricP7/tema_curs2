5109bf3360de7b64b3730c8bfa4d7c04036b3e61
eric <ericpredit@gmail.com>
2025-03-07 17:24:47 +0200

    prob 15 tereminata

--- exercitii.java
+++ exercitii.java
@@ -198,6 +198,17 @@ public class exercitii {
 				if(i+j == 1000)
 					System.out.println(i + " + " + j);
 		}
+		
+		
+		
+		//problema15
+		n=0;
+		for(i=1; i<=9; i++) 
+			for(j=1; j<=9; j++) {
+				n = 3000 + i*100 + 20 + j;
+				if(n%9 == 0)
+					System.out.println(n);
+			}
 	}
 
 }
